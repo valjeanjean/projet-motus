1754d431fdae8bf5208e9ca23e7a226eb6825c5d
valjeanjean <ilian.metina83@gmail.com>
2025-05-22 22:36:41 +0200

    MAJ db.ts

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import mysql from "mysql2/promise";
+
+const db = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user: "motus_user",
+    password: "motus_laplateforme",
+    database: "projet_motus"
+});
+
+export default db;

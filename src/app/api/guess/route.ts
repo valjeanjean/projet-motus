67bd029ffb513cce3e1fde2c96670dc607f4b71b
valjeanjean <ilian.metina83@gmail.com>
2025-05-27 23:31:03 +0200

    addnew

--- src/app/api/guess/route.ts
+++ src/app/api/guess/route.ts
@@ -1,9 +1,46 @@
-import { NextResponse, NextRequest } from "next/server";
 import db from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2"
 
-export default function handleGuess(req: NextApiRequest, res: NextApiResponse){
-
+export default async function handleGuess(req: NextApiRequest, res: NextApiResponse){
+ 
+    /* 
     
+    Envoyer dans la requête le playerID préalablement stocké dans
+    le local storage (Faire la fonctionnalité)
+    
+    */
+
+   const fakePlayerID = 1;
+
+   const guess = req.body;
+
+    const [allRows]: any = await db.execute(
+
+        'SELECT word FROM Game WHERE playerID = ?', [fakePlayerID]
+    );
+
+    const wordToFind = allRows[0].word;
+    const wordToFindArray = wordToFind.split("");
+
+    if(guess === wordToFind){
+
+        res.json({isCorrect: true}); 
+        // On renvoie un objet pour dire que le mot trouvé est le bon
+    }
+
+    guess.forEach((letter: any, index:number) => {
+        
+        if(letter == wordToFindArray[index]){
+
+            console.log("Bonne lettre pour la position " + index + " de la ligne");
+        
+        }else{
+
+            console.log("Mauvaise lettre pour la position " + index + " de la ligne");
+
+        }
+
+    });
 
 }

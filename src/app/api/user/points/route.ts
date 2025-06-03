7223427792d7728e1a2fe84c4a39de33010b1463
valjeanjean <ilian.metina83@gmail.com>
2025-06-03 23:52:53 +0200

    v2.01

--- src/app/api/user/points/route.ts
+++ src/app/api/user/points/route.ts
@@ -1,1 +1,26 @@
 // Faire la route pour update les points
+import db from "@/lib/db";
+import { NextResponse, NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function POST(req: NextRequest){
+
+    console.log("Réponse route /points");
+    const response = await req.json();
+
+    const pointsEarned = response.points;
+    if(!pointsEarned || pointsEarned < 1){
+
+    
+
+        console.log("Erreur de récupération des points gagnés");
+        return NextResponse.json({message: "Erreur de récupération des points gagnés"}, {status: 401});
+    }
+
+    
+
+    
+
+
+
+}
